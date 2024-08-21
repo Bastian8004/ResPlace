1a6c75c14f598ad06c08bcf77447598ca467aecb
Bastian8004 <123553744+Bastian8004@users.noreply.github.com>
2024-08-21 18:00:31 +0200

    Update urls.py

--- Res/urls.py
+++ Res/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('book/', views.book_lane, name='book_lane'),
-path('confirm/<int:reservation_id>/', views.reservation_confirm, name='reservation_confirm'),  # New URL pattern
+path('confirm/<int:reservation_id>/', views.reservation_confirm, name='reservation_confirm'),  
 ]
